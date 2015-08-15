4b059a0831a179f1fdd29f4851c2ea5ed61149b8
Basith Mohammed <basith374@gmail.com>
2015-08-15 15:10:31 +0530

    starting motors

---
+++ app/Motor.php
@@ -0,0 +1,17 @@
+<?php namespace App;
+/**
+ * Created by PhpStorm.
+ * User: RAMU
+ * Date: 15-08-2015
+ * Time: 12:50
+ */
+
+
+
+class Motor extends Product {
+
+    protected $table = "motors";
+
+    protected $guarded = ["id"];
+
+}
