c83a6b6645821d39594910992bd78aa329181a64
waxx04 <rinasmusthafa@gmail.com>
2015-08-21 00:22:03 +0530

    css fixes

--- app/Http/Controllers/AdminPanelController.php
+++ app/Http/Controllers/AdminPanelController.php
@@ -42,7 +42,7 @@ class AdminPanelController extends Controller {
     public function __construct(AdminPanelRepository $adminpanel)
     {
         $this->adminPanel = $adminpanel;
-//        $this->middleware('admin');
+        $this->middleware('admin');
     }
 
     public function dashboard()
